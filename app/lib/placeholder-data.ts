2a113a6483f44e7b5cf5f61c046eacee971ef675
Ermias Bulcha <ermiasbulcha93@gmail.com>
2025-09-25 14:54:09 +0300

    doing in database

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -12,19 +12,19 @@ const users = [
 const customers = [
   {
     id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
+    name: 'bulcha',
     email: 'evil@rabbit.com',
     image_url: '/customers/evil-rabbit.png',
   },
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
+    name: 'bura bure',
     email: 'delba@oliveira.com',
     image_url: '/customers/delba-de-oliveira.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
+    name: 'remy bulcha',
     email: 'lee@robinson.com',
     image_url: '/customers/lee-robinson.png',
   },
--- app/query/route.ts
+++ app/query/route.ts
@@ -1,13 +1,13 @@
-// import postgres from 'postgres';
+ import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
+async function listInvoices() {
+	const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
 //   `;
 
 // 	return data;
@@ -18,9 +18,9 @@ export async function GET() {
     message:
       'Uncomment this file and remove this line. You can delete this file when you are finished.',
   });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
 }
